b73f3bd1d3dac0e8c17c607304819a9bbdca3cb3
Ashish Malpani <ashish76@vt.edu>
2018-04-06 20:55:37 -0400

    Working prototype 2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var YQL = require('yql');
 const hostname = '127.0.0.1';
 const port = 8080;
 
-temperature = 0;
+var temperature = 0;
 
 function getTemperature(zip, isFahrenheitScale) {
   cmd = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" +zip +"')"
@@ -19,35 +19,39 @@ function getTemperature(zip, isFahrenheitScale) {
     var condition = data.query.results.channel.item.condition;
   
     temperature = condition.temp;
-    // console.log(temperature);
   });
 }
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  // res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Type', 'application/json');
-
   var url_parts = url.parse(req.url, true);
+  var path = url_parts.path;
+  var path_array = path.split("/");
   var query = url_parts.query;
 
-  /* Default scale is F */
-  isFahrenheitScale = query.scale==='Celsius'?0:1;
-  scale=isFahrenheitScale?"Fahrenheit":"Celsius";
-  zip = '24060';
+  if(path_array.length < 3 || path_array[1]!="locations"){
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
 
-  getTemperature(zip, isFahrenheitScale);
-  console.log(isFahrenheitScale);
-  console.log(temperature);
 
-  payload = {
-    temperature:temperature,
-    scale:scale
-  };
-  
-  console.log(payload);
+    res.end('Usage <a href="http://localhost:8080/locations/24060">http://localhost:8080/locations/24060</a>');
+  }else{
+    /* Default scale is F */
+    var isFahrenheitScale = query.scale==='Celsius'?0:1;
 
-  res.end(JSON.stringify(payload));
+    var zip = path_array[2];
+    getTemperature(zip, isFahrenheitScale);
+
+    var payload = {
+      temperature:temperature,
+      scale:isFahrenheitScale?"Fahrenheit":"Celsius"
+    };
+
+    console.log(payload);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(payload));
+  }
 });
 
 server.listen(port, hostname, () => {
