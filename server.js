079ef8e599a5861dbd25bcb02a21c1b65476d466
Ashish Malpani <ashish76@vt.edu>
2018-04-07 17:04:33 -0400

    working final build

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
 const http = require('http');
-const url = require('url');
-const YQL = require('yql');
+var url = require('url');
+var YQL = require('yql');
 
 const hostname = '127.0.0.1';
 const port = 8080;
-
-var temperature = 0;
-
-function getTemperature(zip, isFahrenheitScale) {
-  cmd = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" +zip +"')"
-  if(!isFahrenheitScale){
-    cmd += " AND u='c'";
-  }
-
-  var query = new YQL(cmd);
-
-  query.exec(function(err, data) {
-    var condition = data.query.results.channel.item.condition;
-  
-    temperature = condition.temp;
-  });
-}
 
 const server = http.createServer((req, res) => {
   var url_parts = url.parse(req.url, true);
@@ -32,25 +15,43 @@ const server = http.createServer((req, res) => {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/html');
 
-
-    res.end('Usage <a href="http://localhost:8080/locations/24060">http://localhost:8080/locations/24060</a>');
-  }else{
+    var usage = 'Usage:\
+    <br>\
+    <a href="http://localhost:8080/locations/24060">http://localhost:8080/locations/24060</a>\
+    <br>\
+    <a href="http://localhost:8080/locations/24060?scale=Celsius">http://localhost:8080/locations/24060?scale=Celsius</a>';
+    res.end(usage);
+  }
+  else{
     /* Default scale is F */
     var isFahrenheitScale = query.scale==='Celsius'?0:1;
 
     var zip = path_array[2];
-    getTemperature(zip, isFahrenheitScale);
 
-    var payload = {
-      temperature:temperature,
-      scale:isFahrenheitScale?"Fahrenheit":"Celsius"
-    };
 
-    console.log(payload);
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(payload));
+    cmd = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" +zip +"')"
+    if(!isFahrenheitScale){
+      cmd += " AND u='c'";
+    }
+  
+    var query = new YQL(cmd);
+  
+    query.exec(function(err, data) {
+      var condition = data.query.results.channel.item.condition;
+    
+      temperature = condition.temp;
+      
+      var payload = {
+        temperature:temperature,
+        scale:isFahrenheitScale?"Fahrenheit":"Celsius"
+      };
+  
+      console.log(payload);
+  
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(payload));
+    });
   }
 });
 
